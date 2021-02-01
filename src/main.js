e3887d3b7066074d9fb94379e978bf651e20e1c0
brettbrosius <brettbrosius11@gmail.com>
2021-02-01 23:32:32 +0000

    Added red faces

--- src/main.js
+++ src/main.js
@@ -1,10 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".card").innerHTML = generateRandomFace();
+};
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+let generateRandomFace = () => {
+  let faces = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexFaces = Math.floor(Math.random() * faces.length);
+  return faces[indexFaces];
+};
+let generateRandomSuit = () => {
+  let suits = ["diamond", "heart", "club", "spade"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
 };
